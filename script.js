1251e208923fe2319da85ff9fa011031eedf94be
islom <islomodilov573@gmail.com>
2026-04-03 16:08:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const namespace = "siren-gift-unique";
+// Counter
+const namespace = "siren-gift-global";
 
 fetch(`https://api.countapi.xyz/hit/${namespace}/visits`)
   .then(res => res.json())
@@ -6,3 +7,23 @@ fetch(`https://api.countapi.xyz/hit/${namespace}/visits`)
     document.getElementById("counter").innerText =
       `🌸 ${data.value} people have seen this`;
   });
+
+// Name personalization
+const params = new URLSearchParams(window.location.search);
+const name = params.get("name");
+if (name) {
+  document.getElementById("title").innerText =
+    `This is for you, ${name} 💜`;
+}
+
+// Play button
+const video = document.getElementById("bg-video");
+const music = document.getElementById("bg-music");
+const playBtn = document.getElementById("playBtn");
+
+playBtn.addEventListener("click", () => {
+  video.muted = false; // video ovozini yoqish
+  video.play();
+  music.play();
+  playBtn.style.display = "none"; // tugmani yashirish
+});
